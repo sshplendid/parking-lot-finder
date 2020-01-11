27ca754d02c42c635900d36fbc6bf9924785ed41
SeHun Shin <s.shin@marathoner.kr>
2020-01-11 18:26:38 +0900

    chore: Spring Boot 설정

---
+++ script.sh
@@ -0,0 +1,1 @@
+docker run --name mysql -p 3306:3306 -v /Users/sshplendid/WALTZ/docker/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=admin1! -e MYSQL_DATABASE=parkinglot -d mysql:5.7
---
+++ src/main/java/me/shawn/challenge/parkinglotapi/ParkinglotApiApplication.java
@@ -0,0 +1,13 @@
+package me.shawn.challenge.parkinglotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkinglotApiApplication.class, args);
+    }
+
+}
