b4791f825dce71be9e3fcb4b06cc2a6ac8fc815b
SeHun Shin <s.shin@marathoner.kr>
2020-01-12 17:33:05 +0900

    feat: 컨트롤러 파라미터 필수여부 설정 변경

--- src/main/java/me/shawn/challenge/parkinglotapi/v1/park/ParkController.java
+++ src/main/java/me/shawn/challenge/parkinglotapi/v1/park/ParkController.java
@@ -17,7 +17,7 @@ public class ParkController {
     }
 
     @GetMapping("/{address}")
-    public List<ParkInfoDTO> getParkingLotByAddress(@PathVariable String address, @RequestParam(required = false) String tel, @RequestParam(required = false) String parkingName, @RequestParam(defaultValue = "1") int rowStartAt, @RequestParam(defaultValue = "30") int rowEntAt) {
+    public List<ParkInfoDTO> getParkingLotByAddress(@PathVariable String address, @RequestParam(required = false) String tel, @RequestParam(required = false) String parkingName, @RequestParam(required = false, defaultValue = "1") int rowStartAt, @RequestParam(required = false, defaultValue = "30") int rowEntAt) {
         return parkService.getParkInfoByAddress(address, rowStartAt, rowEntAt, tel, parkingName);
     }
 
