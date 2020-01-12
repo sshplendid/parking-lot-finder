1cde1600b795a8a117706bdafa64eaab166bb252
SeHun Shin <s.shin@marathoner.kr>
2020-01-12 15:51:02 +0900

    chore: Docker MySQL 설정 및 스키마 변경

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
-docker run --name mysql -p 3306:3306 -v $(pwd)/.docker/mysql:/var/lib/mysql \
+docker run --name mysql -p 3306:3306 \
 -e MYSQL_ROOT_PASSWORD=admin1! \
 -e MYSQL_DATABASE=parkinglot \
 -d mysql:5.7 \
---lower_case_table_names=1
+--lower_case_table_names=1 \
+--character-set-server=utf8 \
+--collation-server=utf8_unicode_ci
+#-v $(pwd)/.docker/mysql:/var/lib/mysql \
