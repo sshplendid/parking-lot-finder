c2081155e2a6386681ef79933b233d717ecc6a59
SeHun Shin <s.shin@marathoner.kr>
2020-01-12 12:11:37 +0900

    chore: mysql docker 스크립트 수정 및 스키마 생성

--- script.sh
+++ script.sh
@@ -1,1 +1,5 @@
-docker run --name mysql -p 3306:3306 -v /Users/sshplendid/WALTZ/docker/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=admin1! -e MYSQL_DATABASE=parkinglot -d mysql:5.7
+docker run --name mysql -p 3306:3306 -v $(pwd)/.docker/mysql:/var/lib/mysql \
+-e MYSQL_ROOT_PASSWORD=admin1! \
+-e MYSQL_DATABASE=parkinglot \
+-d mysql:5.7 \
+--lower_case_table_names=1
