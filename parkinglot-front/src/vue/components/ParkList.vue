c06887b67f326c63d37c54e33478f2fd78cdd90f
SeHun Shin <s.shin@marathoner.kr>
2020-01-14 01:04:47 +0900

    chore: build.gradle 수정

---
+++ parkinglot-front/src/vue/components/ParkList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <
+  </div>
+</template>
+
+<script>
+import Park from "./Park";
+export default {
+    components: {
+      Park
+    }
+}
+</script>
+
+<style>
+
+</style>
