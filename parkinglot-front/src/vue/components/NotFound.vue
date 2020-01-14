dd81e96daf1f53e2f699e3f1e1b81461ad7ca0ce
SeHun Shin <s.shin@marathoner.kr>
2020-01-15 03:14:16 +0900

    feat: SPA로 작성 및 부트스트랩 적용

---
+++ parkinglot-front/src/vue/components/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="list-group-item list-group-item-action">
+      <h5 class="mb-1">검색 결과가 없습니다.</h5>
+      <p class="mb-1">검색 조건을 변경해서 시도해주세요.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: []
+}
+</script>
+
+<style>
+
+</style>
