41dec34e2543319c2407498a413935ea7717d29b
SeHun Shin <shshin12@lotte.net>
2020-01-17 19:31:48 +0900

    test: 주차가능여부 테스트
    도커환경에서 서버 타임존으로 인한 테스트 중 (WIP)

--- parkinglot-api/src/test/java/me/shawn/challenge/parkinglotapi/openapi/model/ParkInfoDTOTest.java
+++ parkinglot-api/src/test/java/me/shawn/challenge/parkinglotapi/openapi/model/ParkInfoDTOTest.java
@@ -2,8 +2,15 @@ package me.shawn.challenge.parkinglotapi.openapi.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ParkInfoDTOTest {
 
@@ -55,5 +62,42 @@ class ParkInfoDTOTest {
         // THEN
         System.out.println(uniqueKey);
         assertThat(uniqueKey).isEqualTo("1568867-037.5550823000-126.9003791500");
+    }
+
+    @Test
+    void getParkingFeePerHour() {
+        // GIVEN
+        // WHEN
+        // THEN
+    }
+
+    @Test
+    void testGetUniqueKey() {
+        // GIVEN
+        // WHEN
+        // THEN
+    }
+
+    @Test
+    void isParkable_주차되어야하는데_주차가안됨() {
+        // GIVEN
+        Clock clock = Clock.fixed(Instant.parse("2020-01-17T19:12:24Z"), ZoneId.of("UTC"));
+        ParkInfoDTO park = ParkInfoDTO.builder()
+                .curParking(0d)
+                .capacity(54d)
+                .holidayBeginTime("0000")
+                .holidayEndTime("0000")
+                .weekdayBeginTime("1200")
+                .weekdayEndTime("2200")
+                .weekendBeginTime("1200")
+                .weekendEndTime("1800")
+                .build();
+//        when(LocalDateTime.now()).thenReturn(LocalDateTime.of(2020, 1, 17, 19, 12, 24));
+//        LocalDateTime now = LocalDateTime.now();
+//        System.out.println(now);
+//        assertThat(now).isEqualToIgnoringSeconds(LocalDateTime.ofInstant(clock.instant(), ZoneId.of("UTC")));
+
+        // WHEN and THEN
+        assertThat(park.isParkable()).isTrue();
     }
 }
--- proxy/src/main/java/me/shawn/challenge/proxy/ProxyApplication.java
+++ proxy/src/main/java/me/shawn/challenge/proxy/ProxyApplication.java
@@ -1,13 +1,37 @@
 package me.shawn.challenge.proxy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ProxyApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(ProxyApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(ProxyApplication.class, args);
+    }
+
+    @Autowired
+    ApplicationContext context;
+
+    @Bean
+    CommandLineRunner debugForDocker(@Value("${spring.cloud.gateway.routes[0].id}") String id, @Value("${spring.cloud.gateway.routes[0].uri}") String uri) {
+        return args -> {
+            log.info("Current Time: {}", LocalDateTime.now());
+            log.info("TimeZone: {}", TimeZone.getDefault());
+            log.info("id: {}, uri: {}", id, uri);
+        };
     }
 
 }
